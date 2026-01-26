d3fb143d9223390d7dc708fa4757e8606bf7e83a
Gennady Evstratov <g.evstratov@gmail.com>
2026-01-27 02:37:32 +0300

    feat: bug: progress counter exceeds total

---
+++ internal/runner/progress_before_fix_test.go
@@ -0,0 +1,10 @@
+package runner
+
+package runner
+
+import "testing"
+
+// Placeholder
+func TestProgressCounter_Placeholder2(t *testing.T) {
+	t.Skip("Placeholder")
+}
---
+++ internal/runner/progress_bug_reproduction_test.go
@@ -0,0 +1,8 @@
+package runner
+
+import "testing"
+
+// Simple placeholder test
+func TestProgressCounter_Placeholder(t *testing.T) {
+	t.Skip("Placeholder test")
+}
