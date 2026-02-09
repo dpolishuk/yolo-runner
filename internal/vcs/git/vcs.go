be10d6a804de389a53c4daf74fa107cf86cb360f
Gennady Evstratov <g.evstratov@gmail.com>
2026-02-09 21:18:53 +0000

    test commit

--- internal/vcs/git/vcs.go
+++ internal/vcs/git/vcs.go
@@ -29,25 +29,31 @@ func (a *VCSAdapter) CreateTaskBranch(ctx context.Context, taskID string) (strin
 	return branch, nil
 }
 
-func (a *VCSAdapter) Checkout(context.Context, string) error {
-	// Branch switch strategy is explicit through EnsureMain/CreateTaskBranch in v2.
-	return nil
+func (a *VCSAdapter) Checkout(_ context.Context, ref string) error {
+	_, err := a.runner.Run("git", "checkout", ref)
+	return err
 }
 
-func (a *VCSAdapter) CommitAll(context.Context, string) (string, error) {
-	return "", nil
+func (a *VCSAdapter) CommitAll(_ context.Context, message string) (string, error) {
+	return a.commitAll(message)
 }
 
-func (a *VCSAdapter) MergeToMain(context.Context, string) error {
-	return nil
+func (a *VCSAdapter) MergeToMain(ctx context.Context, sourceBranch string) error {
+	if err := a.EnsureMain(ctx); err != nil {
+		return err
+	}
+	_, err := a.runner.Run("git", "merge", "--no-ff", sourceBranch)
+	return err
 }
 
-func (a *VCSAdapter) PushBranch(context.Context, string) error {
-	return nil
+func (a *VCSAdapter) PushBranch(_ context.Context, branch string) error {
+	_, err := a.runner.Run("git", "push", "-u", "origin", branch)
+	return err
 }
 
 func (a *VCSAdapter) PushMain(context.Context) error {
-	return nil
+	_, err := a.runner.Run("git", "push", "origin", "main")
+	return err
 }
 
 func (a *VCSAdapter) commitAll(message string) (string, error) {
